9a2ceb0c428778d9efb3f25a448143d56a66c2a6
aaron@ajbosley.co.uk <aaron@ajbosley.co.uk>
2018-06-09 11:14:12 +0100

    Initial commit

---
+++ routes/views/index.js
@@ -0,0 +1,43 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'Home';
+	locals.data = {
+		posts: [],
+		categories: [],
+	};
+	// Load the posts
+	view.on('init', function (next) {
+
+		var q = keystone.list('Post').paginate({
+			page: req.query.page || 1,
+			perPage: 6,
+			maxPages: 1,
+			filters: {
+				state: 'published',
+			},
+		})
+			.sort('-publishedDate')
+			.populate('author categories');
+
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.category]);
+		}
+
+		q.exec(function (err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+	});
+
+
+
+	// Render the view
+	view.render('index');
+};
